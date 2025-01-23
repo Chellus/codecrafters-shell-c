8545f180b9a99e61b579e49ef1e3f8657769a49b
Chellus <elias4845@gmail.com>
2025-01-23 11:42:59 -0300

    repl implementation

--- app/main.c
+++ app/main.c
@@ -2,19 +2,22 @@
 #include <string.h>
 
 int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-
-  // remove the trailing newline
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
-
+  
+  
+  while (1) {// Flush after every printf
+    setbuf(stdout, NULL);
+  
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+  
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+  
+    // remove the trailing newline
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
+  
   return 0;
 }
